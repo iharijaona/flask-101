947c4e12c9adc1b2a2796ea1c6395d7514152047
HarryJohn <harijaona@digital.gov.mg>
2021-12-21 15:34:25 +0300

    Delete product

--- tests/test_views.py
+++ tests/test_views.py
@@ -14,8 +14,20 @@ class TestViews(TestCase):
     
     def test_read_one_product(self):
         response = self.client.get("/api/v1/products/1")
+        product = response.json
+        self.assertIsInstance(product, dict)
         self.assertEqual(response.status_code, 200)
     
     def test_read_unknown_product(self):
         response = self.client.get("/api/v1/products/55")
         self.assertEqual(response.status_code, 404)
+    
+    def test_delete_one_product(self):
+        response = self.client.delete("/api/v1/products/2")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json["success"], True)
+    
+    def test_delete_unknown_product(self):
+        response = self.client.delete("/api/v1/products/55")
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.json["success"], False)
