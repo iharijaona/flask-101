3247a480e09a789a33a5272550743201affd12da
HarryJohn <harijaona@digital.gov.mg>
2021-12-21 12:37:32 +0300

    Adding /api/v1/products endpoint

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,18 @@
 from flask import Flask
+from flask import jsonify
+
 app = Flask(__name__)
+
+PRODUCTS = {
+    1: { 'id': 1, 'name': 'Skello' },
+    2: { 'id': 2, 'name': 'Socialive.tv' },
+}
 
 @app.route('/')
 def hello():
     return "<b>Hello World!</b>"
+
+
+@app.route('/api/v1/produits')
+def get_produits():
+    return jsonify(PRODUCTS)
